2a1ed3629e70dfc86770303729b9de69dcbdccf8
erika <erypaltas@gmail.com>
2020-06-03 13:22:30 -0500

    Add config Heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const app = express();
 const desarrolladorRoutes = require('./routes/desarrollador');
 
 // settings
+
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,11 +22,11 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(conn(mysql, {
-    host: 'localhost',
-    user: 'root',
-    password: 'erika',
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'bc356af8b9cba9',
+    password: '6dc7859f',
     port: 3306,
-    database: 'db_desarrolladores'
+    database: 'heroku_3fc8e50e0bcef7a'
 }, 'single'))
 
 // Routes
